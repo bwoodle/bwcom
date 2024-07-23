c0f7486229a2fed0ca90c2837e60b42219eaea27
Brent Woodle <bwoodle@gmail.com>
2024-07-22 19:42:11 -0500

    test AND prod now work

--- bwcom-backend-cdk/src/CdkBwcomBackend/CdkBwcomBackendStack.cs
+++ bwcom-backend-cdk/src/CdkBwcomBackend/CdkBwcomBackendStack.cs
@@ -12,10 +12,6 @@ namespace CdkBwcomBackend
     {
         internal CdkBwcomBackendStack(Construct scope, string id, StackProps props = null) : base(scope, id, props)
         {
-            var rootDomain = "brentwoodle.com";
-            var zone = HostedZone.FromLookup(this, "bwcom", new HostedZoneProviderProps(){ DomainName = rootDomain});
-
-            // Define the Lambda function resource
             var helloWorldFunction = new Function(this, "BwcomData", new FunctionProps
             {
                 Runtime = Runtime.NODEJS_20_X, // Choose any supported Node.js runtime
@@ -23,46 +19,60 @@ namespace CdkBwcomBackend
                 Handler = "hello.handler" // Points to the 'hello' file in the lambda directory
             });
 
-            // Define the API Gateway resource
             var api = new LambdaRestApi(this, "bwcom-api", new LambdaRestApiProps
             {
                 Deploy = false,
-                DomainName = new DomainNameOptions()
-                {
-                    DomainName = "bwcom-api.brentwoodle.com",
-                    Certificate = Certificate.FromCertificateArn(this, "bwcom-cert", "arn:aws:acm:us-east-1:685339315795:certificate/3ab367af-a156-481c-934b-47e65da78c4e")
-                },
                 Handler = helloWorldFunction,
                 Proxy = false
             });
+            CreateApiResources(api);
 
-            var deployment = new Deployment(this, "bwcom-deployment", new DeploymentProps{
+            var deployment = new Deployment(this, "bwcom-deployment", new DeploymentProps
+            {
                 Api = api
             });
 
-            new Amazon.CDK.AWS.APIGateway.Stage(this, "bwcom-test-stage", new Amazon.CDK.AWS.APIGateway.StageProps
-            {
-                Deployment = deployment,
-                StageName = "test",
+            var zone = HostedZone.FromLookup(this, "bwcom", new HostedZoneProviderProps() { DomainName = "brentwoodle.com" });
+            var cert = Certificate.FromCertificateArn(this, $"bwcom-cert", "arn:aws:acm:us-east-1:685339315795:certificate/3ab367af-a156-481c-934b-47e65da78c4e");
+            CreateStage(api, deployment, zone, cert, "prod", "bwcom-api");
+            CreateStage(api, deployment, zone, cert, "test", "bwcom-test-api");
+        }
 
-            });
-
-            new Amazon.CDK.AWS.APIGateway.Stage(this, "bwcom-prod-stage", new Amazon.CDK.AWS.APIGateway.StageProps
-            {
-                Deployment = deployment,
-                StageName = "prod"
-            });
-
-            new ARecord(this, "bwcom-api-dns", new ARecordProps
-            {
-                Zone = zone,
-                RecordName = "bwcom-api",
-                Target = RecordTarget.FromAlias(new ApiGateway(api))
-            });
-
-            // Add a '/hello' resource with a GET method
+        private void CreateApiResources(IRestApi api)
+        {
             var helloResource = api.Root.AddResource("hello");
             helloResource.AddMethod("GET");
+        }
+
+        private void CreateStage(IRestApi api, Deployment deployment, IHostedZone zone, ICertificate certificate, string stageName, string dns)
+        {
+            var prodStage = new Amazon.CDK.AWS.APIGateway.Stage(this, $"bwcom-{stageName}-stage", new Amazon.CDK.AWS.APIGateway.StageProps
+            {
+                Deployment = deployment,
+                StageName = stageName
+            });
+
+            var prodDomain = new DomainName_(this, $"bwcom-{stageName}-domain", new DomainNameProps
+            {
+                DomainName = $"{dns}.brentwoodle.com",
+                Certificate = certificate
+            });
+
+            new BasePathMapping(this, $"bwcom-api-{stageName}-mapping", new BasePathMappingProps
+            {
+                DomainName = prodDomain,
+                Stage = prodStage,
+                RestApi = api
+            });
+
+            var domain = new ApiGatewayDomain(prodDomain);
+
+            new ARecord(this, $"bwcom-{stageName}-dns", new ARecordProps
+            {
+                Zone = zone,
+                RecordName = dns,
+                Target = RecordTarget.FromAlias(domain)
+            });
         }
     }
 }
