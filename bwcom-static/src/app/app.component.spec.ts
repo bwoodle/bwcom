992d36c0ca8b1a9bc3327d1923b8fd84c6ec4740
Brent Woodle <bwoodle@gmail.com>
2024-07-23 20:45:17 -0500

    Fix tester

--- bwcom-static/src/app/app.component.spec.ts
+++ bwcom-static/src/app/app.component.spec.ts
@@ -1,16 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ApiService } from './api/api.service';
+import { WINDOW_PROVIDERS } from 'src/utils/window.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientModule
       ],
       declarations: [
         AppComponent
       ],
+      providers: [
+        HttpClient, WINDOW_PROVIDERS
+      ]
     }).compileComponents();
   });
 
@@ -18,18 +25,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'bwcom'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('bwcom');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('bwcom app is running!');
   });
 });
