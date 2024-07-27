892a46648220eec65ea3335374c220b752dceb31
Brent Woodle <bwoodle@gmail.com>
2024-07-27 14:46:13 -0500

    test Alias now hits latest version

--- bwcom-backend-cdk/src/CdkBwcomBackend/DataFunctions/DataFunctionsStack.cs
+++ bwcom-backend-cdk/src/CdkBwcomBackend/DataFunctions/DataFunctionsStack.cs
@@ -1,4 +1,5 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.CodeDeploy;
 using Amazon.CDK.AWS.Lambda;
 using Constructs;
 
@@ -6,7 +7,6 @@ namespace CdkBwcomBackend.FunctionsStack
 {
   public class DataFunctionsStack : Stack
   {
-    public readonly Function HelloWorld;
     public readonly Alias HelloWorldTest;
     public readonly Alias HelloWorldProd;
 
@@ -18,9 +18,20 @@ namespace CdkBwcomBackend.FunctionsStack
         Code = Code.FromAsset("lambda"), // Points to the lambda directory
         Handler = "hello.handler" // Points to the 'hello' file in the lambda directory
       });
-      HelloWorld = fn;
-      HelloWorldTest = fn.AddAlias("test");
-      HelloWorldProd = fn.AddAlias("prod");
+
+      HelloWorldTest = new Alias(this, "BwcomTestData", new AliasProps
+      {
+        AliasName = "test",
+        Description = "Test data integration",
+        Version = fn.LatestVersion
+      });
+
+      HelloWorldProd = new Alias(this, "BwcomProdData", new AliasProps
+      {
+        AliasName = "prod",
+        Description = "Prod data integration",
+        Version = fn.CurrentVersion
+      });
     }
   }
 }
