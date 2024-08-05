4cf0221b03588602ef3d1892964f07914a3a1274
Brent Woodle <bwoodle@gmail.com>
2024-08-04 19:52:34 -0500

    Angular 18 Upgrade

--- bwcom-static/src/app/app.component.spec.ts
+++ bwcom-static/src/app/app.component.spec.ts
@@ -1,23 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { WINDOW_PROVIDERS } from 'src/services/window.service';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        HttpClientModule
-      ],
-      declarations: [
+    declarations: [
         AppComponent
-      ],
-      providers: [
-        HttpClient, WINDOW_PROVIDERS
-      ]
-    }).compileComponents();
+    ],
+    imports: [RouterTestingModule],
+    providers: [
+        HttpClient, WINDOW_PROVIDERS,
+        provideHttpClient(withInterceptorsFromDi())
+    ]
+}).compileComponents();
   });
 
   it('should create the app', () => {
--- bwcom-static/src/app/app.module.ts
+++ bwcom-static/src/app/app.module.ts
@@ -3,19 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { WINDOW_PROVIDERS } from 'src/services/window.service';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule
-  ],
-  providers: [HttpClient, WINDOW_PROVIDERS],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule], providers: [HttpClient, WINDOW_PROVIDERS, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
