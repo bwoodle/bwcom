2400fba89855fca01ad0cba336c8c5d315f4d895
Brent Woodle <bwoodle@gmail.com>
2025-03-29 21:08:46 -0500

    Fix unit tests for standalone components

--- bwcom-static/src/app/app.component.spec.ts
+++ bwcom-static/src/app/app.component.spec.ts
@@ -1,25 +1,32 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { WINDOW_PROVIDERS } from 'src/services/window.service';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { WINDOW, WINDOW_PROVIDERS } from 'src/services/window.service';
+import { provideOAuthClient } from 'angular-oauth2-oidc';
+import { CommonModule } from '@angular/common';
+import { BaseHrefService } from 'src/services/base-href.service';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [RouterTestingModule],
-    providers: [
+      imports: [AppComponent, CommonModule],
+      providers: [
         HttpClient, WINDOW_PROVIDERS,
-        provideHttpClient(withInterceptorsFromDi())
-    ]
-}).compileComponents();
+        BaseHrefService,
+        provideHttpClient(),
+        provideOAuthClient()
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
--- bwcom-static/src/components/race-history/race-history.component.spec.ts
+++ bwcom-static/src/components/race-history/race-history.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RaceHistoryComponent } from './race-history.component';
+import { provideHttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { HelloWorldService } from 'src/services/hello-world.service';
+import { WINDOW, WINDOW_PROVIDERS } from 'src/services/window.service';
 
 describe('RaceHistoryComponent', () => {
   let component: RaceHistoryComponent;
@@ -8,9 +12,10 @@ describe('RaceHistoryComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RaceHistoryComponent]
+      imports: [RaceHistoryComponent, CommonModule],
+      providers: [provideHttpClient(), HelloWorldService, WINDOW_PROVIDERS]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(RaceHistoryComponent);
     component = fixture.componentInstance;
